199b3a7c27afdfc3494f1853c73e162cbbdabbe0
Tiphaine HOGREL <hogrelt@gmail.com>
2022-07-24 12:46:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import"./Style.css";
 
-function App() {
+export default function App() {
+
+  const [anim, setAnime] = useState(false);
+
+  const classToggle = () => {
+    setAnime(!anim)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`box ${anim? "animclasse": ""}`} />
+      <button onClick={classToggle}> change la classe</button>
     </div>
   );
 }
 
-export default App;
